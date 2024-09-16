69f84359cd3e175ced36d603c6c51754c09c1f13
jordanmowry <jordan.mowry@gmail.com>
2024-09-16 12:14:43 -0400

    feat: refactor portfolio item into component
    PortfolioItem is now a component to avoid code
    duplicaton.

---
+++ components/PortfolioItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex mb-24 portfolio" :style="{ gap: 'var(--gap)' }">
+    <NuxtImg
+      :src="imageSrc"
+      :alt="altText"
+      width="300"
+      height="300"
+      class="portfolio-screenshot rounded-full"
+    />
+    <div>
+      <h3 class="title">{{ title }}</h3>
+      <p class="text" v-for="(paragraph, index) in description" :key="index">
+        {{ paragraph }}
+      </p>
+      <a :href="link" class="btn btn--line" target="_blank">Visit</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  imageSrc: string;
+  altText: string;
+  title: string;
+  description: string[];
+  link: string;
+}
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+.portfolio {
+  display: flex;
+  gap: var(--gap);
+  margin-bottom: 100px;
+}
+
+.portfolio-screenshot {
+  border-radius: 50%;
+  width: clamp(175px, 20vw, 300px);
+  height: clamp(175px, 20vw, 300px);
+}
+</style>
