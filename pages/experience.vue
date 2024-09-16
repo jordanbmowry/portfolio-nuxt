0a7b54aae8e75551223ce63396a37d2620cfc105
jordanmowry <jordan.mowry@gmail.com>
2024-09-16 12:16:44 -0400

    chore: refactor
    loop through skills to avoid code duplication.

--- pages/experience.vue
+++ pages/experience.vue
@@ -8,33 +8,16 @@
       <div class="max-w-5xl px-5 mx-auto">
         <h2 id="heading-experience" class="title--sm mb-9">Experience</h2>
         <ul class="pl-0">
-          <li class="tag">React</li>
-          <li class="tag">Vue</li>
-          <li class="tag">Nuxt</li>
-          <li class="tag">Agile</li>
-          <li class="tag">HTML</li>
-          <li class="tag">CSS</li>
-          <li class="tag">JavaScript</li>
-          <li class="tag">Tailwind</li>
-          <li class="tag">SQL</li>
-          <li class="tag">PostgreSQL</li>
-          <li class="tag">MongoDB</li>
-          <li class="tag">Node.js</li>
-          <li class="tag">Express</li>
-          <li class="tag">Fastify</li>
-          <li class="tag">Python</li>
-          <li class="tag">Prompt Engineering</li>
-          <li class="tag">Docker</li>
-          <li class="tag">Kubernetes</li>
-          <li class="tag">CI/CD</li>
-          <li class="tag">AWS</li>
-          <li class="tag">Serverless</li>
-          <li class="tag">Cloud Computing</li>
-          <li class="tag">OutSystems</li>
+          <li
+            v-for="(skill, index) in experienceItems"
+            :key="index"
+            class="tag"
+          >
+            {{ skill }}
+          </li>
         </ul>
-        <!-- Download CV Button -->
         <NuxtLink
-          class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full"
+          class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-500/50 dark:text-zinc-300 dark:hover:bg-zinc-500 dark:hover:text-zinc-50 dark:active:bg-zinc-500 dark:active:text-zinc-50/70 group mt-6 w-full"
           to="#"
           @click="downloadResume"
         >
@@ -59,9 +42,35 @@
 </template>
 
 <script setup>
+const experienceItems = [
+  'React',
+  'Vue',
+  'Nuxt',
+  'Agile',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Tailwind',
+  'SQL',
+  'PostgreSQL',
+  'MongoDB',
+  'Node.js',
+  'Express',
+  'Fastify',
+  'Python',
+  'Prompt Engineering',
+  'Docker',
+  'Kubernetes',
+  'CI/CD',
+  'AWS',
+  'Serverless',
+  'Cloud Computing',
+  'OutSystems',
+];
+
 const downloadResume = () => {
   const link = document.createElement('a');
-  link.href = '/Jordan_Resume.pdf'; //
+  link.href = '/Jordan_Resume.pdf';
   link.download = 'Jordan_Resume.pdf';
   link.click();
 };
